e401ada0dc2a5581c32ee980b4040a85c248cd2b
Samuel Van Ackere <samuel.vanackere@vlaanderen.be>
2022-11-16 13:54:49 +0100

    update Apache NIFI flow

--- timescaledb/iow.sql
+++ timescaledb/iow.sql
@@ -5,13 +5,13 @@ CREATE TABLE IF NOT EXISTS public.water_quality
 (
     id character varying NOT NULL,
     "batterijniveau_value" double precision, 
-    "batterijniveau_date" date,
+    "batterijniveau_date" text,
     "batterijniveau_sensor" character varying,
     "temperatuur_value" double precision,
-    "temperatuur_date" date,
+    "temperatuur_date" text,
     "temperatuur_sensor" character varying,
     "conductiviteit_value"  double precision,
-    "conductiviteit_date" date,
+    "conductiviteit_date" text,
     "conductiviteit_sensor"  character varying
 );
 
